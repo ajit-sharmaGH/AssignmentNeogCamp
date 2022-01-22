7574fb3b14cfad1789fe0bdac4510f56a13c2485
AJIT SHARMA <ajit08082000@gmail.com>
2022-01-22 18:12:25 +0530

    done with the badge task

---
+++ badge.js
@@ -0,0 +1,7 @@
+const buttonHide = document.querySelector(".button-hide");
+const badgeSelector = document.querySelector(".badge-container-type-four");
+buttonHide.addEventListener("click", () => {
+    badgeSelector.style.display = "none";
+
+
+});
