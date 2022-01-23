3ba66ba59fc6f2294df03c5cd442b1681be01821
AJIT SHARMA <ajit08082000@gmail.com>
2022-01-24 03:07:43 +0530

    debug Css

--- badge.js
+++ badge.js
@@ -1,5 +1,5 @@
 const buttonHide = document.querySelector(".button-hide");
-const badgeSelector = document.querySelector(".badge-container-type-four");
+const badgeSelector = document.querySelector(".badge-type-four");
 buttonHide.addEventListener("click", () => {
     badgeSelector.style.display = "none";
 
